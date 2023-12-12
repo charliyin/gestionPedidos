e31c04687f8d24eaa93871a33e2eb19ff871a06d
charly <carchuss@hotmail.com>
2023-12-11 23:32:57 -0300

    first commit

---
+++ ProyectoWeb/contacto/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render, redirect
+from .forms import FormularioContacto
+from django.core.mail import EmailMessage
+
+def contacto(request):
+    formulario_contacto=FormularioContacto()
+    if request.method=="POST":
+        formulario_contacto=FormularioContacto(data=request.POST)
+        if formulario_contacto.is_valid():
+            nombre=request.POST.get("nombre")
+            email=request.POST.get("email")
+            contenido=request.POST.get("contenido")
+
+            email=EmailMessage("Mensaje desde App Django", 
+            "Usuario nombre {} dirección email {} envía: \n\n {}".format(nombre,email,contenido),
+            "",["carchuss@hotmail.com"], reply_to=[email])
+            try: 
+                email.send()
+                return redirect("/contacto/?valido")
+            except:
+                return redirect("/contacto/?novalido")
+
+    return render(request,"contacto/contacto.html", {'miFormulario':formulario_contacto})
